7df593aaf904285332d047f130bd5f296d1f717c
Lucas Maiz <lmaiz@neoxia.deloitte.fr>
2024-11-13 15:48:39 +0100

    update import

--- dataflow-pipelines/flex-pokemon-pubsub-dataflow-biqquery/tests/test_pokemon_processing.py
+++ dataflow-pipelines/flex-pokemon-pubsub-dataflow-biqquery/tests/test_pokemon_processing.py
@@ -5,7 +5,7 @@ from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 
-from app.ptransform import PokemonProcessing
+from app.utils import PokemonProcessing
 
 
 class TestPokemonProcessing(unittest.TestCase):
