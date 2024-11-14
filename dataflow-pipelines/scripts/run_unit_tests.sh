2b55a4264007c8445b921506a009802f8da88746
Lucas Maiz <lmaiz@neoxia.deloitte.fr>
2024-11-14 16:44:10 +0100

    refacto

--- dataflow-pipelines/scripts/run_unit_tests.sh
+++ dataflow-pipelines/scripts/run_unit_tests.sh
@@ -11,8 +11,7 @@ if [ -s /workspace/changed_folders ]; then
 
         # Check if the 'tests' folder exists and is not empty
         if [[ -d tests && -n $(ls -A tests) ]]; then
-        for file in tests/*; do
-            if [[ -f $file && $file == test*.py]]; then
+        for file in $(find tests -type f -name "test*.py"); do
             # Run tests for each Python file found
             echo "Running tests for $file"
             python -m tests/$file
